335a96940b0c18918bd646ca8fd983eeeceda23d
Khushi Garg <khushigarg.64901@gmail.com>
2023-05-01 18:02:39 +0530

    added:put and delete
     - put and delete return promise that resolved on response OK

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,50 +1,99 @@
 const URL = "http://gdrive.vasaniyakush.tech";
 const get = async (path, payload) => {
-  return new Promise(async (resolve, reject) => {
-    try {
-      const response = await fetch(
-        URL + path + new URLSearchParams(payload).toString(),
-        {
-          method: "GET",
-          mode: "cors",
-          cache: "no-cache",
-          credentials: "same-origin",
-          headers: {
-            "Content-Type": "application/json",
-            // 'Content-Type': 'application/x-www-form-urlencoded',
-          },
-          redirect: "follow",
-          referrerPolicy: "no-referrer",
-        }
-      );
+    return new Promise(async (resolve, reject) => {
+        try {
+            const response = await fetch(
+                URL + path + new URLSearchParams(payload).toString(),
+                {
+                    method: "GET",
+                    mode: "cors",
+                    cache: "no-cache",
+                    credentials: "same-origin",
+                    headers: {
+                        "Content-Type": "application/json",
+                        // 'Content-Type': 'application/x-www-form-urlencoded',
+                    },
+                    redirect: "follow",
+                    referrerPolicy: "no-referrer",
+                }
+            );
 
-      resolve(response.json());
-    } catch (err) {
-      reject(err);
-    }
-  });
+            resolve(response.json());
+        } catch (err) {
+            reject(err);
+        }
+    });
 };
 const post = (path, payload) => {
-  return new Promise(async (resolve, reject) => {
-    try {
-      const response = await fetch(URL + path, {
-        method: "POST",
-        mode: "cors",
-        cache: "no-cache",
-        credentials: "same-origin",
-        headers: {
-          "Content-Type": "application/json",
-          // 'Content-Type': 'application/x-www-form-urlencoded',
-        },
-        redirect: "follow",
-        referrerPolicy: "no-referrer",
-        body: JSON.stringify(payload),
-      });
+    return new Promise(async (resolve, reject) => {
+        try {
+            const response = await fetch(URL + path, {
+                method: "POST",
+                mode: "cors",
+                cache: "no-cache",
+                credentials: "same-origin",
+                headers: {
+                    "Content-Type": "application/json",
+                    // 'Content-Type': 'application/x-www-form-urlencoded',
+                },
+                redirect: "follow",
+                referrerPolicy: "no-referrer",
+                body: JSON.stringify(payload),
+            });
 
-      resolve(response.json());
-    } catch (err) {
-      reject(err);
-    }
-  });
+            resolve(response.json());
+        } catch (err) {
+            reject(err);
+        }
+    });
 };
-export { get, post };
+
+const put = async (path, payload) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            const response = await fetch(URL + path, {
+                method: "PUT",
+                mode: "cors",
+                cache: "no-cache",
+                credentials: "same-origin",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                redirect: "follow",
+                referrerPolicy: "no-referrer",
+                body: JSON.stringify(payload),
+            });
+
+            resolve(response.json());
+        } catch (err) {
+            reject(err);
+        }
+    });
+};
+
+const remove = async (path, payload) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            const response = await fetch(
+                URL + path + new URLSearchParams(payload).toString(),
+                {
+                    method: "DELETE",
+                    mode: "cors",
+                    cache: "no-cache",
+                    credentials: "same-origin",
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                    redirect: "follow",
+                    referrerPolicy: "no-referrer",
+                }
+            );
+            resolve(response.json());
+        } catch (err) {
+            reject(err);
+        }
+    });
+};
+
+
+export { get, post, put, remove };
